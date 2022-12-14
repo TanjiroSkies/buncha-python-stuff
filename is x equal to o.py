5717c7795eb38c6aaef752ea81fdbd6d16c17f2a
TanjiroSkies <94139744+TanjiroSkies@users.noreply.github.com>
2022-12-14 20:41:02 -0300

    is X equal to O

---
+++ is x equal to o.py
@@ -0,0 +1,15 @@
+string = "XOX"
+
+string_X = string.count("X")
+
+string_O = string.count("O")
+
+if string_X > string_O:
+
+    print("false")
+
+elif string_X <= string_O:
+
+    print("true")
+
+
